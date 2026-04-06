5779f7de56d8553e5c8c1578ac0d6853961dc432
carlostapia <carlostapiaT3csup32@gmailcom>
2026-04-06 10:46:52 -0500

    prueba final

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ terraform {
     resource_group_name  = "rg-apppersonal-tfstate"
     storage_account_name = "stcarlosv3state"
     container_name       = "tfstate-apppersonal"
-    key                  = "tfstate.v6"
+    key                  = "tfstate.v7"
   }
 }
 
--- variables.tf
+++ variables.tf
@@ -19,7 +19,7 @@ variable "aks_name" {
 }
 
 variable "apim_name" {
-  default = "apimcarlos69lmv6"
+  default = "apimcarlos69lmv7"
 }
 
 variable "sql_server_name" {
